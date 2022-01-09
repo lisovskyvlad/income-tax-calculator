0822df43cdcd1fd4485d6cb0cc57994d1255c8ce
Vladislav Lisovskii <vladislav.lisovskii@lingoda.com>
2022-01-09 16:20:29 +0100

    Initial commit

---
+++ src/einkommensteuer/core.clj
@@ -0,0 +1,6 @@
+(ns einkommensteuer.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/einkommensteuer/core_test.clj
@@ -0,0 +1,7 @@
+(ns einkommensteuer.core-test
+  (:require [clojure.test :refer :all]
+            [einkommensteuer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
