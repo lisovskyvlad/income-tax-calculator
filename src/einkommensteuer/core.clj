6b4749c6a4a6b96d906317235ba7aab7502db042
Vladislav Lisovskii <vladislav.lisovskii@lingoda.com>
2022-01-09 17:44:04 +0100

    Add last bracket

--- src/einkommensteuer/core.clj
+++ src/einkommensteuer/core.clj
@@ -25,12 +25,11 @@
     Est))
 
 (comment
-  (bracket1 50000) ;; => 11884.9496781068
-  (bracket1 58000) ;; => 15093.117009306801
+  (bracket2 50000) ;; => 11884.9496781068
+  (bracket2 58000) ;; => 15093.117009306801
   )
 
-;; d) 58597 € bis 277825 €:
-;; ESt = 0.42 * zvE - 9.267.53;
+;; d) 58597 € bis 277825 €: ESt = 0.42 * zvE - 9.267.53;
 (defn bracket3 [zvE]
   (- (* 0.42 zvE) 9267.52))
 
@@ -39,10 +38,25 @@
   (bracket3 270000) ;; => 104132.48
   )
 
+;; e) ab 277.826 €: ESt = 0.45 * zvE - 17602.28
+(defn bracket4 [zvE]
+  (- (* 0.45 zvE) 17602.28))
+
+(comment
+  (bracket4 300000) ;; => 117397.72
+  (bracket4 1000000) ;; => 432397.72
+  )
+
 (defn income-tax [einkommen]
   (cond
     (< einkommen 9984) (bracket0)
     (< 9985 einkommen 14926) (bracket1 einkommen)
     (< 14927 einkommen 58596) (bracket2 einkommen)
     (< 58597 einkommen 277825 ) (bracket3 einkommen)
-    :else "zero"))
+    :else (bracket4 einkommen)))
+
+(comment
+  (income-tax 36000) ;; => 6906.4612485068
+  (income-tax 70000) ;; => 20132.48
+  (income-tax 950000);; => 409897.72
+  )
