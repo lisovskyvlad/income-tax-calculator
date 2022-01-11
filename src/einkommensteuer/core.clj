9fc2012528790867680fd501c7f335d4495a11f9
Vladislav Lisovskii <vladislav.lisovskii@lingoda.com>
2022-01-11 22:26:10 +0100

    Add round2 function, update values with precisions 2

--- src/einkommensteuer/core.clj
+++ src/einkommensteuer/core.clj
@@ -1,5 +1,12 @@
 (ns einkommensteuer.core)
 
+;; http://clojure-doc.org/articles/language/functions.html
+(defn round2
+  "Round a double to the given precision (number of significant digits)"
+  [precision d]
+  (let [factor (Math/pow 10 precision)]
+    (/ (Math/round (* d factor)) factor)))
+
 ;; a) bis 9984 € (Grundfreibetrag): 0;
 (defn bracket0 []
   0)
@@ -10,10 +17,10 @@
   (let [y (/ (- zvE 9984) 10000.0)
         yy (+ (* 1008.7 y) 1400)
         Est (* yy y)]
-    Est))
+    (round2 2 Est)))
 
 (comment
-  (bracket1 14000) ;; => 724.925718272
+  (bracket1 14000) ;; => 724.93
   )
 
 ;; c) 14927 € bis 58596 €:
@@ -22,11 +29,11 @@
   (let [z (/ (- zvE 14926) 10000.0)
         zz (+ (* 206.43 z) 2397)
         Est (+ (* zz z) 938.24)]
-    Est))
+    (round2 2 Est)))
 
 (comment
-  (bracket2 50000) ;; => 11884.9496781068
-  (bracket2 58000) ;; => 15093.117009306801
+  (bracket2 50000) ;; => 11884.95
+  (bracket2 58000) ;; => 15093.12
   )
 
 ;; d) 58597 € bis 277825 €: ESt = 0.42 * zvE - 9.267.53;
@@ -34,6 +41,7 @@
   (- (* 0.42 zvE) 9267.52))
 
 (comment
+  (bracket3 67000) ;; => 18872.48
   (bracket3 100000) ;; => 32732.48
   (bracket3 270000) ;; => 104132.48
   )
@@ -52,11 +60,17 @@
     (< einkommen 9984) (bracket0)
     (< 9985 einkommen 14926) (bracket1 einkommen)
     (< 14927 einkommen 58596) (bracket2 einkommen)
-    (< 58597 einkommen 277825 ) (bracket3 einkommen)
-    :else (bracket4 einkommen)))
+    (< 58597 einkommen 277825) (bracket3 einkommen)
+    :else
+    (bracket4 einkommen)))
 
 (comment
-  (income-tax 36000) ;; => 6906.4612485068
+  (income-tax 36000) ;; => 6906.46
   (income-tax 70000) ;; => 20132.48
-  (income-tax 950000);; => 409897.72
+  (income-tax 950000) ;; => 409897.72
   )
+
+
+(->> (range 8000 50000 1000)
+    ;;  (take 10)
+     (map (fn [einkommen] [einkommen, (income-tax einkommen)])))
