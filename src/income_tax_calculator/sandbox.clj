03c4171ed404ddd9af8b4dbf17e22d160eb578e3
Vladislav Lisovskii <vladislav.lisovskii@klarna.com>
2022-05-22 19:08:03 +0200

    new_commit

--- src/income_tax_calculator/sandbox.clj
+++ src/income_tax_calculator/sandbox.clj
@@ -1,12 +1,15 @@
-(ns income-tax-calculator.sandbox)
+(ns income-tax-calculator.sandbox
+  (:require [clojure.pprint :as pp]))
+
 
 (def discountData {:text  {:prefix "Up to"
-                            {:body} "$99"
-                            :suffix "disc"}
-                    :color "#000"})
+                           :body "$99"
+                           :suffix "disc"}
+                   :color "#000"})
 
 ;; Clojure:
-(assoc-in discountData [:text :body] "$10")
+(pp/pprint
+ (assoc-in discountData [:text :body] "$10"))
 
 ;; JavaScript:
 ;; export const discountData = {
@@ -14,3 +17,5 @@
 ;;   color: '#000',
 ;; }
 ;; {...discountData, text: {...discountData.text, body: "$10"}}
+
+(sort ["size" "title"])
