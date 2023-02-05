985a9ac88c6c0b725a0111700ed109354397dc7d
Vladislav Lisovskii <vladislav.lisovskii@on-running.com>
2023-02-05 15:12:01 +0100

    sandbox game

--- src/income_tax_calculator/sandbox.clj
+++ src/income_tax_calculator/sandbox.clj
@@ -1,6 +1,6 @@
 (ns income-tax-calculator.sandbox
-  (:require [clojure.pprint :as pp]))
-
+  (:require [clojure.pprint :as pp]
+            [clojure.data.json :as json]))
 
 (def discountData {:text  {:prefix "Up to"
                            :body "$99"
@@ -11,11 +11,23 @@
 (pp/pprint
  (assoc-in discountData [:text :body] "$10"))
 
+(json/read-str "{\"a\":1,\"b\":2}" :key-fn str) ;; => {"a" 1, "b" 2}
+(json/read-str "{\"a\":1,\"b\":2}" :key-fn #(keyword "com.example" %)) ;; => #:com.example{:a 1, :b 2}
+(json/write-str {:a 1 :b 2}) ;; => "{\"a\":1,\"b\":2}"
+
+;; Shitty JS
+;; > JSON.stringify({"keyX": undefined, "keyY": "value"})
+;; '{"keyY":"value"}'
+
+(json/write-str {"kryX" nil "keyY" "value"})
+;; => "{\"kryX\":null,\"keyY\":\"value\"}"
+
+(json/read-str "{\"a\":1,\"b\":2}")
+;;=> {"a" 1, "b" 2}
+
 ;; JavaScript:
 ;; export const discountData = {
 ;;   text: {  prefix: 'Up to', body: '50%', suffix: 'off' },
 ;;   color: '#000',
 ;; }
 ;; {...discountData, text: {...discountData.text, body: "$10"}}
-
-(sort ["size" "title"])
